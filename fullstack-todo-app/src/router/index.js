0e9b1dfb8c354d895ebc3c63fc6a7ec385c4e483
Rorins <aurora268@hotmail.it>
2023-09-01 09:54:59 +0200

    redirection to home

--- fullstack-todo-app/src/router/index.js
+++ fullstack-todo-app/src/router/index.js
@@ -8,6 +8,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/login',
+    },
+    {
       path: '/sign-up',
       name: 'signUp',
       component: SignUp
